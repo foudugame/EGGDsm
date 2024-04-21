2b183043d9075b475a476acab5181d6c5cd69b0e
foudugame360 <158186555+foudugame@users.noreply.github.com>
2024-04-22 01:46:20 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ imgboot="${dir}/arc-flat.vmdk"
 ram=3G
 core=2
 sizeStockage=2000G
-    TAG="$(curl -s https://api.github.com/repos/AuxXxilium/arc/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')"
+    TAG="$(wget -qO- https://api.github.com/repos/AuxXxilium/arc/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')"
     wget "https://github.com/AuxXxilium/arc/releases/download/${TAG}/arc-${TAG}.vmdk-dyn.zip" -O "arc-vmdk.zip"
 
     if [ ! -f "$hdd" ]; then
